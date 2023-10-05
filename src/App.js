227ebb80c067ebc2689e12c14a63ce9817a15c2e
Morgan Mastrangelo <morgan172cm@gmail.com>
2023-10-05 09:25:23 -0400

    complete initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.css';
+
+import Navbar from './layouts/Navbar';
+import Header from './layouts/Header';
+import About from './layouts/About';
+import Values from './layouts/Values';
+import Works from './layouts/Works';
+import Hiring from './layouts/Hiring';
+import Subscribe from './layouts/Subscribe';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex flex-col justify-center items-center">
+      <Navbar />
+      <Header />
+      <About />
+      <Values />
+      <Works />
+      <Hiring />
+      <Subscribe />
+      <Footer />
+    </main>
   );
 }
 
